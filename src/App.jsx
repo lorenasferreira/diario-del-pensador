1f626a3cccdf289a104e96f18354f403ae39dd6a
GeraldineSaco <gsacoruiz@gmail.com>
2025-12-16 19:53:05 +0100

    feat:creation of button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +10,10 @@ function App() {
         <nav></nav>
       </header>
       <main>
-          <section></section>
+        
+        <section>
+
+        </section>
           <section></section>
       </main>
       <footer></footer>
