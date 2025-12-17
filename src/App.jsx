2fa63a334fd85b810d54ca64a2fa51c0483e2d37
Sukaina <sukisu91@gmail.com>
2025-12-17 14:35:28 +0100

    feat: add id to phrases

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import useState from"./components/QuotesList";
 
 function App() {
 
--- src/components/QuotesList.jsx
+++ src/components/QuotesList.jsx
@@ -23,52 +23,64 @@ import { useState } from "react"
 const QuotesList = () => {
     const [phrase, setPhrase] = useState([
         {
+          id:1,
             text: "Quien sea feliz también hará feliz a otros",
             autor: "Anne Frank"
         },
         {
+          id:2,
             text: "La educación es el arma más poderosa para cambiar el mundo",
             autor: "Nelson Mandela"
         },
         {
+          id:3,
             text: "Dormir es distraerse del mundo",
             autor: "Jorge Luis Borges"
         },
         {
+          id:4,
             text: "Un día sin reír es un día perdido",
             autor: "Charles Chaplin"
         },
         {
+          id:5,
             text: "Sé el cambio que quieres ver en el mundo",
             autor: "Mahatma Gandhi"
-        }
+        },
     ])
     return (
         <>
           {
-            phrase.map(phrase, index) => (
-              <div key="index">
+            phrase.map((phrase, index) => (
+              <div key={phrase,index}>
                 <h2>Frases guardadas</h2>
                 <h4>Una colección de pensamientos que quieres tener cerca</h4>
                 <button>Mas</button>
-                <div>
+                
+
+                <div key={phrase.id1}>
                   <p>{phrase.text} {phrase.autor} </p>
                   <button></button>
                   <button></button>
                 </div>
-                <div>
+
+                   <div key={phrase.id2}>
                   <p>{phrase.text} {phrase.autor} </p>
                   <button></button>
                   <button></button>
                 </div>
-                <div>
+
+                  <div key={phrase.id3}>
                   <p>{phrase.text} {phrase.autor} </p>
                   <button></button>
                   <button></button> 
-                </div>
+                  
+                 <div key={phrase.id4}>
+                <p>{phrase.text} {phrase.autor} </p>
               </div>
+               </div>
             )
-          
+         
 
         </>
 
